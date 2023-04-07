7c01f8a52647c349cc49c2551017f2b962286bd9
Kushal Beniwal <kushal.beniwal@nirmata.com>
2023-04-07 14:08:05 +0530

    move AMIs from instances to nodegroups
    Signed-off-by: Kushal Beniwal <kushal.beniwal@nirmata.com>

--- api/v1alpha1/zz_generated.deepcopy.go
+++ api/v1alpha1/zz_generated.deepcopy.go
@@ -574,10 +574,12 @@ func (in *EKSNodeGroup) DeepCopyInto(out *EKSNodeGroup) {
 		*out = make([]string, len(*in))
 		copy(*out, *in)
 	}
-	if in.InstanceTypes != nil {
-		in, out := &in.InstanceTypes, &out.InstanceTypes
-		*out = make([]string, len(*in))
-		copy(*out, *in)
+	if in.AmazonMachineImages != nil {
+		in, out := &in.AmazonMachineImages, &out.AmazonMachineImages
+		*out = make([]AmazonMachineImage, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
 	}
 	if in.UpdateConfig != nil {
 		in, out := &in.UpdateConfig, &out.UpdateConfig
